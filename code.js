591c2d56d15e5c6f49b1ae1e15b4760247059666
graham619408-stack <graham.619408@swl.k12.oh.us>
2025-09-29 18:47:46 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
-var max = 9;
+const max = 9;
 var number = 50;
 
 var rng = Math.floor(Math.random() * max)
 
 var balance = document.getElementById("balance")
 balance.textContent = number
+
+var slot1 = document.getElementById("slot1")
+slot1.textContent = rng
