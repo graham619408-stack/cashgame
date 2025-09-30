4447455740c65f7f43b1f333f3a001a178bd9a73
graham619408-stack <graham.619408@swl.k12.oh.us>
2025-09-30 18:51:57 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
 const max = 9;
-var number = 50;
+var cash = 1;
 
-var rng = Math.floor(Math.random() * max)
+var rng = Math.floor(Math.random() * max);
 
-var balance = document.getElementById("balance")
-balance.textContent = number
+const balance = document.getElementById("balance");
+balance.textContent = number;
 
-var slot1 = document.getElementById("slot1")
-slot1.textContent = rng
+const button = document.getElementById("button");
+
+function click() {
+  cash += 1;
+}
