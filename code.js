3c1a197c01c3a40cf3dc712b1e92824627d7d750
graham619408-stack <graham.619408@swl.k12.oh.us>
2025-09-30 18:56:32 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,15 @@
-const max = 9;
 var cash = 1;
 
 var rng = Math.floor(Math.random() * max);
 
 const balance = document.getElementById("balance");
-balance.textContent = number;
+balance.textContent = cash;
 
 const button = document.getElementById("button");
 
 function click() {
   cash += 1;
+  balance.textContent = cash;
+
+  alert("Check!")
 }
